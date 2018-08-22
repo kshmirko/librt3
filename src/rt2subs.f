f3217e1b064a0a6cf30b4ad892819d56b0ff0d85
kshmirko <shmirko.konstantin@gmail.com>
2018-08-23 08:53:31 +1000

    Внесены изменения в команды вызова расчетов

--- src/rt2subs.f
+++ src/rt2subs.f
@@ -500,3 +500,168 @@ C      CLOSE (3)
 
       RETURN
       END
+
+
+      SUBROUTINE OUTPUT_RADS (NSTOKES, NUMMU, AZIORDER,
+     .                    SRC_CODE, LAYER_FILE, OUT_FILE,
+     .                    QUAD_TYPE, DELTAM, DIRECT_FLUX, DIRECT_MU,
+     .                    GROUND_TEMP, GROUND_TYPE,
+     .                    GROUND_ALBEDO, GROUND_INDEX,
+     .                    SKY_TEMP, WAVELENGTH, UNITS, OUTPOL,
+     .                    NUM_LAYERS, HEIGHT,
+     .                    NOUTLEVELS, OUTLEVELS, NUMAZIMUTHS,
+     .                    MU_VALUES, PHIVAL, UP_FLUX, DOWN_FLUX,
+     .                    UP_RAD, DOWN_RAD, UP, DOWN) BIND(C)
+      use iso_c_binding
+      IMPLICIT NONE
+      INTEGER  NSTOKES, NUMMU, NUMAZI, AZIORDER, SRC_CODE, NUM_LAYERS
+      INTEGER  NOUTLEVELS, OUTLEVELS(*), NUMAZIMUTHS
+      REAL*8   GROUND_TEMP, GROUND_ALBEDO
+      REAL*8   SKY_TEMP, WAVELENGTH
+      REAL*8   DIRECT_FLUX, DIRECT_MU
+      REAL*8   HEIGHT(NUM_LAYERS+1)
+      REAL*8   MU_VALUES(NUMMU), PHIVAL(NUMAZIMUTHS)
+      REAL*8   UP_FLUX(NSTOKES,NOUTLEVELS)
+      REAL*8   DOWN_FLUX(NSTOKES,NOUTLEVELS)
+      REAL*8   UP_RAD(NSTOKES,NUMMU,AZIORDER+1,NOUTLEVELS)
+      REAL*8   DOWN_RAD(NSTOKES,NUMMU,AZIORDER+1,NOUTLEVELS)
+      REAL*8   UP(NSTOKES, NUMMU, NUMAZIMUTHS, NOUTLEVELS)
+      REAL*8   DOWN(NSTOKES, NUMMU, NUMAZIMUTHS, NOUTLEVELS)
+      COMPLEX*16  GROUND_INDEX
+      CHARACTER*(*) LAYER_FILE, OUT_FILE
+      CHARACTER QUAD_TYPE*1, DELTAM*1, UNITS*1, OUTPOL*2, GROUND_TYPE*1
+      CHARACTER*32 QUAD_NAME, UNITS_NAME, GROUND_NAME
+      CHARACTER*64 FORM1
+      INTEGER  I, J, K, L, LI, M, N
+      REAL*4   OUT(4), PHI, PHID, PI
+      PARAMETER (PI=3.1415926535897932384D0)
+      external CONVERT_OUTPUT
+
+      N = NUMMU*(AZIORDER+1)*NOUTLEVELS
+      CALL CONVERT_OUTPUT (UNITS, OUTPOL, NSTOKES, N, 
+     .                     WAVELENGTH, 0, UP_RAD)
+      CALL CONVERT_OUTPUT (UNITS, OUTPOL, NSTOKES, N, 
+     .                     WAVELENGTH, 0, DOWN_RAD)
+      CALL CONVERT_OUTPUT (UNITS, OUTPOL, NSTOKES, NOUTLEVELS, 
+     .                     WAVELENGTH, 1, UP_FLUX)
+      CALL CONVERT_OUTPUT (UNITS, OUTPOL, NSTOKES, NOUTLEVELS, 
+     .                     WAVELENGTH, 1, DOWN_FLUX)
+
+      NUMAZI = 2*AZIORDER+1
+      IF (NSTOKES .LE. 2) NUMAZI = AZIORDER+1
+      QUAD_NAME = 'GAUSSIAN'
+      IF (QUAD_TYPE .EQ. 'D')  QUAD_NAME = 'DOUBLEGAUSS'
+      IF (QUAD_TYPE .EQ. 'L')  QUAD_NAME = 'LOBATTO'
+      IF (QUAD_TYPE .EQ. 'E')  QUAD_NAME = 'EXTRA-ANGLES'
+      UNITS_NAME = 'WATTS/(M^2 MICRON STER)'
+      IF (UNITS .EQ. 'T') UNITS_NAME = 'KELVINS - EBB'
+      IF (UNITS .EQ. 'R') UNITS_NAME = 'KELVINS - RJ'
+      GROUND_NAME = 'LAMBERTIAN'
+      IF (GROUND_TYPE .EQ. 'F')  GROUND_NAME = 'FRESNEL'
+
+      
+
+C C           Output the parameters
+C       WRITE (3,'(A,I3,A,I3,A,I3,A,I1)')
+C      .                'C  NUMMU=', NUMMU,  '  NUMAZI=',NUMAZI,
+C      .                '  AZIORDER=',AZIORDER, '  NSTOKES=',NSTOKES
+C       WRITE (3,'(A,A32,A,A1)')
+C      .                'C  LAYER_FILE=',    LAYER_FILE,
+C      .                '   DELTA-M=',DELTAM
+C       WRITE (3,'(A,I1,A,A16)')
+C      .                'C  SRC_CODE=',      SRC_CODE,
+C      .                '   QUAD_TYPE=',     QUAD_NAME
+C       IF (SRC_CODE .EQ. 1 .OR. SRC_CODE .EQ. 3) THEN
+C           WRITE (3,'(A,E11.5,A,F8.6)')
+C      .                'C  DIRECT_FLUX=',   DIRECT_FLUX,
+C      .                '   DIRECT_MU=',     DIRECT_MU
+C       ENDIF
+C       WRITE (3,'(A,F8.2,A,A16)')
+C      .                'C  GROUND_TEMP=',   GROUND_TEMP,
+C      .                '   GROUND_TYPE=',   GROUND_NAME
+C       IF (GROUND_TYPE(1:1) .EQ. 'F') THEN
+C           WRITE (3,'(A,2F9.4,A,F8.2)')
+C      .                'C  GROUND_INDEX=',  GROUND_INDEX,
+C      .                '   SKY_TEMP=',      SKY_TEMP
+C       ELSE
+C           WRITE (3,'(A,F8.5,A,F8.2)')
+C      .                'C  GROUND_ALBEDO=', GROUND_ALBEDO,
+C      .                '   SKY_TEMP=',      SKY_TEMP
+C       ENDIF
+C       WRITE (3,'(A,E12.6)') 'C  WAVELENGTH=',    WAVELENGTH
+C       WRITE (3,'(A,A25,A,A2)') 'C  UNITS='     ,    UNITS_NAME,
+C      .                '   OUTPUT_POLARIZATION=', OUTPOL  
+
+
+      IF (UNITS(1:1) .EQ. 'T') THEN
+          FORM1 = '(F8.3,1X,F5.1,1X,F8.5,4(1X,F7.2),:)'
+      ELSE
+          FORM1 = '(F8.3,1X,F5.1,1X,F8.5,4(1X,E13.6),:)'
+      ENDIF
+ 
+C       IF (OUTPOL .EQ. 'VH') THEN
+C         WRITE (3,'(A)') 
+C      .    'C    Z      PHI     MU    FLUX/RADIANCE (V,H,U,V)'
+C       ELSE
+C         WRITE (3,'(A)') 
+C      .    'C    Z      PHI     MU    FLUX/RADIANCE (I,Q,U,V)'
+C       ENDIF
+ 
+      DO L = 1, NOUTLEVELS
+        LI = OUTLEVELS(L)
+C               Output fluxes at this level
+        WRITE (3,FORM1) HEIGHT(LI), 0., -2.0,
+     .        (SNGL(UP_FLUX(I,L)),I=1,NSTOKES)
+        WRITE (3,FORM1) HEIGHT(LI), 0., +2.0,
+     .        (SNGL(DOWN_FLUX(I,L)),I=1,NSTOKES)
+ 
+C               For each azimuth and zenith at this level sum the Fourier
+C               azimuth series appropriate for the particular Stokes parameter
+C               and output the radiance.
+        DO K = 1, NUMAZIMUTHS
+          IF (NUMAZIMUTHS .EQ. 1) THEN
+            PHID = 0.0
+          ELSE
+            PHID = 180.0*FLOAT(K-1)/(NUMAZIMUTHS-1)
+          ENDIF
+          PHI = PI*PHID/180.0
+          PHIVAL(K) = PHI
+C               Output upwelling radiance: -1 < mu < 0
+          DO J = NUMMU, 1, -1
+            DO I = 1, NSTOKES
+              OUT(I) = 0.0
+              DO M = 0, AZIORDER
+                IF (I .LE. 2) THEN
+                  OUT(I) = OUT(I) + COS(M*PHI)*UP_RAD(I,J,M+1,L)
+                ELSE
+                  OUT(I) = OUT(I) + SIN(M*PHI)*UP_RAD(I,J,M+1,L)
+                ENDIF
+              ENDDO
+            ENDDO
+            UP(1:NSTOKES,J,K,L) = OUT(1:NSTOKES)
+C            WRITE (3,FORM1) HEIGHT(LI), PHID, -MU_VALUES(J),
+C     .                      (OUT(I),I=1,NSTOKES)
+          ENDDO
+C               Output downwelling radiance: 0 < mu < 1
+          DO J = 1, NUMMU
+            DO I = 1, NSTOKES
+              OUT(I) = 0.0
+              DO M = 0, AZIORDER
+                IF (I .LE. 2) THEN
+                  OUT(I) = OUT(I) + COS(M*PHI)*DOWN_RAD(I,J,M+1,L)
+                ELSE
+                  OUT(I) = OUT(I) + SIN(M*PHI)*DOWN_RAD(I,J,M+1,L)
+                ENDIF
+              ENDDO
+            ENDDO
+            DOWN(1:NSTOKES,J,K,L) = OUT(1:NSTOKES)
+C            WRITE (3,FORM1) HEIGHT(LI), PHID,  MU_VALUES(J),
+C     .                      (OUT(I),I=1,NSTOKES)
+          ENDDO
+        ENDDO
+      ENDDO
+
+C      CLOSE (3)
+
+      RETURN
+      END
